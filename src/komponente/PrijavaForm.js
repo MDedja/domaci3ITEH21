36f157b1cad73f84deb591d0cbb2bf58da5e4876
MDedja <milandedjanski@gmail.com>
2022-06-23 12:31:58 +0200

    Dodat objekat useState Hooksa koji prati unete vrednosti u formi za prijavu tima

--- src/komponente/PrijavaForm.js
+++ src/komponente/PrijavaForm.js
@@ -1,4 +1,16 @@
+import {useState} from 'react';
+
 function PrijavaForm(){
+
+    const [tim, setTim] = useState({
+        naziv: '',
+        kapiten: '',
+        igrac1: '',
+        igrac2: '',
+        brojTelefona: '',
+        email: ''
+    });
+
 
     return (
         <div>
@@ -8,18 +20,18 @@ function PrijavaForm(){
 
                 <div className="clan">
                 <label>Naziv tima</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" value={tim.naziv} onChange={e => setTim({...tim, naziv: e.target.value})} />
                 </div>
 
                 <div className="clan">
                 <label>Kapiten</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" value={tim.kapiten} onChange={e => setTim({...tim, kapiten: e.target.value})} />
                 </div>
 
 
                 <div className="clan">
                 <label>Igrač</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" value={tim.igrac1} onChange={e => setTim({...tim, igrac1: e.target.value})}   />
                 </div>
 
                 </div>
@@ -28,17 +40,17 @@ function PrijavaForm(){
 
                 <div className="clan">
                 <label>Igrač</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" value={tim.igrac2} onChange={e => setTim({...tim, igrac2: e.target.value})}  />
                 </div>
 
                 <div className="clan">
                 <label>Broj telefona</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={tim.brojTelefona} onChange={e => setTim({...tim, brojTelefona: e.target.value})}/>
                 </div>
 
                 <div className="clan">
                 <label>Email</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={tim.email} onChange={e => setTim({...tim, email: e.target.value})}/>
                 </div>
 
                 </div>
